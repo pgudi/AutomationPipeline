b2172ff46c6a97940accef851eae9e0c9e4cc328
sgTesting <gopal.gudi@gmail.com>
2024-04-28 08:19:00 +0530

    Pipeline Automation

---
+++ src/test/java/com/sgtesting/tests/unittests/UnitTestcases.java
@@ -0,0 +1,91 @@
+package com.sgtesting.tests.unittests;
+
+import org.testng.annotations.Test;
+
+public class UnitTestcases {
+	
+	@Test(priority = 1)
+	public void method1()
+	{
+		System.out.println("It is method1 unit testcase statement !!!!");
+	}
+
+	@Test(priority = 2)
+	public void method2()
+	{
+		System.out.println("It is method2 unit testcase statement !!!!");
+	}
+	
+	@Test(priority = 3)
+	public void method3()
+	{
+		System.out.println("It is method3 unit testcase statement !!!!");
+	}
+	
+	@Test(priority = 4)
+	public void method4()
+	{
+		System.out.println("It is method4 unit testcase statement !!!!");
+	}
+	
+	@Test(priority = 5)
+	public void method5()
+	{
+		System.out.println("It is method5 unit testcase statement !!!!");
+	}
+	
+	@Test(priority = 6)
+	public void method6()
+	{
+		System.out.println("It is method6 unit testcase statement !!!!");
+	}
+	
+	@Test(priority = 7)
+	public void method7()
+	{
+		System.out.println("It is method7 unit testcase statement !!!!");
+	}
+	
+	
+	@Test(priority = 8)
+	public void method8()
+	{
+		System.out.println("It is method8 unit testcase statement !!!!");
+	}
+	
+	@Test(priority = 9)
+	public void method9()
+	{
+		System.out.println("It is method9 unit testcase statement !!!!");
+	}
+	
+	@Test(priority = 10)
+	public void method10()
+	{
+		System.out.println("It is method10 unit testcase statement !!!!");
+	}
+	
+	@Test(priority = 11)
+	public void method11()
+	{
+		System.out.println("It is method11 unit testcase statement !!!!");
+	}
+	
+	@Test(priority = 12)
+	public void method12()
+	{
+		System.out.println("It is method12 unit testcase statement !!!!");
+	}
+	
+	@Test(priority = 13)
+	public void method13()
+	{
+		System.out.println("It is method13 unit testcase statement !!!!");
+	}
+	
+	@Test(priority = 14)
+	public void method14()
+	{
+		System.out.println("It is method14 unit testcase statement !!!!");
+	}
+}
